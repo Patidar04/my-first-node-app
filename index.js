34d7c532f70f274751cb47d4eacbdcff7ad7dac0
Priya Patidar <your-email@example.com>
2026-04-06 13:21:21 +0530

    fixed package.json

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(<h1>UPDATING ON THE VERCEL SITE</h1>);
-
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
